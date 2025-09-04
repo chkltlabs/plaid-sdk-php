8018715f03770ae3a993a07a10f04b4dd927abbc
chkltlabs <chkltlabs@gmail.com>
2025-09-04 17:12:26 +0100

    feature : Create endpoint updated to Sept 2025 api spec,

--- src/Resources/Tokens.php
+++ src/Resources/Tokens.php
@@ -16,80 +16,96 @@ class Tokens extends AbstractResource
 	 * @param array<string> $country_codes Possible values are: CA, FR, IE, NL, ES, GB, US
 	 * @param User $user
 	 * @param array<string> $products Possible values are: transactions, auth, identity, income, assets, investments, liabilities, payment_initiation
+	 * @param array<string> $required_if_supported_products Possible values are: transactions, auth, identity, income, assets, investments, liabilities, payment_initiation
+	 * @param array<string> $optional_products Possible values are: transactions, auth, identity, income, assets, investments, liabilities, payment_initiation
+	 * @param array<string> $additional_consented_products Possible values are: transactions, auth, identity, income, assets, investments, liabilities, payment_initiation
 	 * @param string|null $webhook
-	 * @param string|null $link_customization_name
-	 * @param AccountFilters|null $account_filters
 	 * @param string|null $access_token
+	 * @param string|null $link_customization_name
 	 * @param string|null $redirect_uri
 	 * @param string|null $android_package_name
-	 * @param string|null $payment_id
+	 * @param array|null $institution_data
+	 * @param AccountFilters|null $account_filters
+	 * @param array|null $eu_config
 	 * @param string|null $institution_id
+	 * @param array|null $payment_initiation
+	 * @param array|null $income_verification
+	 * @param array|null $cra_options
+	 * @param string|null $consumer_report_permissible_purpose
 	 * @param array|null $auth
-	 * @throws PlaidRequestException
+	 * @param array|null $transfer
+	 * @param array|null $update
+	 * @param array|null $identity_verification
+	 * @param array|null $statements
+	 * @param string|null $user_token
+	 * @param array|null $investments
+	 * @param array|null $investments_auth
+	 * @param array|null $hosted_link
+	 * @param array|null $transactions
+	 * @param array|null $identity
+	 * @param bool|null $enable_multi_item_link
+	 * @param string|null $user_id
 	 * @return object
+	 * @throws PlaidRequestException
 	 */
 	public function create(
 		string $client_name,
 		string $language,
 		array $country_codes,
 		User $user,
-		array $products = [],
+		?array $products = [],
+		array $required_if_supported_products = [],
+		array $optional_products = [],
+		array $additional_consented_products = [],
 		?string $webhook = null,
-		?string $link_customization_name = null,
-		?AccountFilters $account_filters = null,
 		?string $access_token = null,
+		?string $link_customization_name = null,
 		?string $redirect_uri = null,
 		?string $android_package_name = null,
-		?string $payment_id = null,
+		?array $institution_data = null,
+		?AccountFilters $account_filters = null,
+		?array $eu_config = null,
 		?string $institution_id = null,
-		?array $auth = null): object {
+		?array $payment_initiation = null,
+		?array $income_verification = null,
+		?array $cra_options = null,
+		?string $consumer_report_permissible_purpose = null,
+		?array $auth = null,
+		?array $transfer = null,
+		?array $update = null,
+		?array $identity_verification = null,
+		?array $statements = null,
+		?string $user_token = null,
+		?array $investments = null,
+		?array $investments_auth = null,
+		?array $hosted_link = null,
+		?array $transactions = null,
+		?array $identity = null,
+		?bool $enable_multi_item_link = null,
+		?string $user_id = null
+	): object {
 
-		$params = [
-			"client_name" => $client_name,
-			"language" => $language,
-			"country_codes" => $country_codes,
-			"user" => $user->toArray(),
-			"products" => $products
-		];
+		//all defined params show here, including ones left null
+		//so, filter out the nulls
+		$params = array_filter(get_defined_vars(), function ($var) {
+			return !empty($var);
+		});
 
-		if( $webhook ){
-			$params["webhook"] = $webhook;
+		//perform any data transformation
+		$params["user"] = $params["user"]->toArray();
+
+		if( isset($params["account_filters"]) ){
+			$params["account_filters"] = $params["account_filters"]->toArray();
 		}
 
-		if( $link_customization_name ){
-			$params["link_customization_name"] = $link_customization_name;
-		}
-
-		if( $account_filters ){
-			$params["account_filters"] = $account_filters->toArray();
-		}
-
-		if( $access_token ){
-			$params["access_token"] = $access_token;
-		}
-
-		if( $redirect_uri ){
-			$params["redirect_uri"] = $redirect_uri;
-		}
-
-		if( $android_package_name ){
-			$params["android_package_name"] = $android_package_name;
-		}
-
-		if( $payment_id ){
+		if( isset($params["payment_id"]) ){
 			$params["payment_initiation"] = [
-				"payment_id" => $payment_id
+				"payment_id" => $params["payment_id"]
 			];
+			unset($params["payment_id"]);
 		}
 
-		if( $institution_id ){
-			$params["institution_id"] = $institution_id;
-		}
-
-		if ($auth) {
-			$params["auth"] = $auth;
-		}
-
+		//pass to plaid
 		return $this->sendRequest(
 			"post",
 			"link/token/create",
